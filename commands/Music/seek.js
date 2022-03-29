25acc2246a0d49bedb2d47a321c207cccb56310a
Nerdify Technologies <102147295+NerdifyTech@users.noreply.github.com>
2022-03-29 17:34:08 +0530

    Add files via upload

---
+++ commands/Music/seek.js
@@ -0,0 +1,60 @@
+const { Command } = require("reconlx");
+const ee = require("../../settings/embed.json");
+const config = require("../../settings/config.json");
+const emoji = require("../../settings/emoji.json");
+const player = require("../../handlers/player");
+const { check_dj } = require("../../handlers/functions");
+
+module.exports = new Command({
+  // options
+  name: "seek",
+  description: ` set seek current song`,
+  userPermissions: ["CONNECT"],
+  botPermissions: ["CONNECT"],
+  category: "Music",
+  cooldown: 5,
+  DJ: true,
+  options: [
+    {
+      name: "amount",
+      description: `Give seek amount in number`,
+      type: "NUMBER",
+      required: true,
+    },
+  ],
+  // command start
+  run: async ({ client, interaction, args, prefix }) => {
+    // Code
+    let channel = interaction.member.voice.channel;
+    let queue = await player.getQueue(interaction.guild.id);
+    if (!channel) {
+      return interaction.followUp(
+        `** ${emoji.ERROR} You Need to Join Voice Channel first **`
+      );
+    } else if (
+      interaction.guild.me.voice.channel &&
+      !interaction.guild.me.voice.channel.equals(channel)
+    ) {
+      return interaction.followUp(
+        `** ${emoji.ERROR} You Need to Join __My__ Voice Channel **`
+      );
+    } else if (interaction.guild.me.voice.serverMute) {
+      return interaction.followUp(
+        `** ${emoji.ERROR} I am Muted in Voice Channel , unmute me first **`
+      );
+    } else if (!queue) {
+      return interaction.followUp(`** ${emoji.ERROR} Nothing Playing Now **`);
+    } else if (check_dj(client, interaction.member, queue.songs[0])) {
+      return interaction.followUp(
+        `** ${emoji.ERROR} You are Not DJ and also not Song Requester **`
+      );
+    } else {
+      let seeksong = interaction.options.getNumber("amount")
+                if (seeksong > queue.songs[0].duration || seeksong < 0) return interaction.reply({
+                    embeds: [
+                        new MessageEmbed().setColor(ee.wrongcolor).setTitle(`${client.allEmojis.x} **The Seek Position must be between \`0\` and \`${Queue.songs[0].duration}\`!**`)
+                    ] });
+await queue.seek(seeksong);
+    }
+  },
+});
